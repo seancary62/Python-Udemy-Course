9eb5d15e5dfefa06a6b1178f218078f5a01ccc4e
Sean Cary <sean.cary62@gmail.com>
2022-12-19 02:22:42 +0000

    Pets exercise

---
+++ functions.py
@@ -0,0 +1,6 @@
+#parameters
+def say_hello(name, emoji):
+  print(f'hello {name} {emoji}')
+
+
+say_hello('Sean', ':^)')
--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-my_list = [1,2,3,4,5,6,7,8,9,10]
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
 
-counter = 0
-for item in my_list:
-  counter = counter + item
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
 
-print(counter)
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+    
+#1 Add nother Cat
+class Stevie(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+      
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = [Stevie('Stevie', 2), Simon('Simon', 3), Sally('Sally',4)]
+
+#3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
+
+#4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
