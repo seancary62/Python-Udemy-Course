e90ef9b4b80fc9066a1ef9cd4e8c9e93b93b350d
Sean Cary <sean.cary62@gmail.com>
2022-12-22 02:44:07 +0000

    list comprehension

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
-from functools import reduce
+my_list = [char for char in 'hellor']
+my_list2 = [num*2 for num in range(0,100)]
+my_list3 = [num**2 for num in range(0,100) if num % 2 == 0]
 
 
+print(my_list3)
 
-#1 Capitalize all of the pet names and print the list
-my_pets = ['sisi', 'bibi', 'titi', 'carla']
-
-print(list(map(lambda item: item.capitalize(), my_pets)))
-
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
-my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
-
-print(list(zip(my_strings,sorted(my_numbers))))
-               
-#3 Filter the scores that pass over 50%
-scores = [73, 20, 65, 19, 76, 100, 88]
-
-print(list(filter(lambda x: x > 50, scores)))
-
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
-
-print(reduce(lambda acc, x: x + acc, my_numbers))
-
-my_list = [5,4,3]
-
-print(list(map(lambda x: x**2, my_list)))
-
-a = [(0,2), (4,3), (9,9), (10,-1)]
-
-a.sort(key=lambda x: x[1])
-
-print(a)
