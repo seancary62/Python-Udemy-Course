40d2a2a2851d64a2f1b149b924adc5be430dc4ce
Sean Cary <sean.cary62@gmail.com>
2022-12-22 02:04:01 +0000

    lambda exercises

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from functools import reduce
 
-my_list = [1,2,3]
-your_list = [10,20,30]
-their_list = [5,4,3]
 
-def multiply_by2(item):
-  return item*2 
 
-def only_odd(item):
-  return item % 2 != 0
+#1 Capitalize all of the pet names and print the list
+my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
-def accumulator(acc, item):
-  print(acc,item)
-  return acc + item
+print(list(map(lambda item: item.capitalize(), my_pets)))
 
-print(list(filter(only_odd, my_list)))
-print(my_list)
-print(list(zip(my_list, your_list)))
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+my_strings = ['a', 'b', 'c', 'd', 'e']
+my_numbers = [5,4,3,2,1]
 
-print(reduce(accumulator, my_list, 0))
+print(list(zip(my_strings,sorted(my_numbers))))
+               
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+
+print(list(filter(lambda x: x > 50, scores)))
+
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+
+print(reduce(lambda acc, x: x + acc, my_numbers))
